b116b6725958248be4640c931f66e216f7edb6bf
Carlos Sales <causalvador@hotmail.com>
2017-09-27 11:26:49 -0300

    host database

--- udpServer.js
+++ udpServer.js
@@ -3,9 +3,9 @@
 //---------------------------------------------------------------------------
 // Banco de Dados SQL
 var con_host = "br-cdbr-azure-south-b.cloudapp.net";         // endereço do host
-var con_user = "bad1618d895dbf";                             // usuario
-var con_pwd = "59b6e38c";                                   // senha
-var con_db = "dbrastrear";                              // nome do banco de dados
+var con_user = "b7bff2566ddc00";                             // usuario
+var con_pwd = "fc6426c2";                                   // senha
+var con_db = "dbrastreio";                              // nome do banco de dados
 
 var s_port = 20500;                 // Porta de comunicaçao UDP
 
@@ -94,7 +94,7 @@ function salva_mensagem(param1, param2, param3, param4) {
         ")";
 
     con.query(sql, function (err, result) {
-        if (err) { console.log("Erro ao Gravar. Detalhes: " + err); return; }
+        if (err) { console.log("Erro ao Gravar. Detalhes: " + err); con.destroy(); return; }
         console.log("Mensagem gravada em Banco de dados.");
     });
 }
@@ -136,7 +136,7 @@ function salva_mensagem_localizacao(loc_msg, loc_adress, loc_port) {
         Latitude = montaLatitude(myarray[6]);
         Longitude = montaLongitude(myarray[7]);
 
-        Speed = myarray[8];
+        Speed = "0"; // myarray[8];
         Course = myarray[9];
 
         MensagemStatus = myarray[10];
@@ -173,7 +173,7 @@ function salva_mensagem_localizacao(loc_msg, loc_adress, loc_port) {
         ")";
 
     con.query(sql, function (err, result) {
-        if (err) { console.log("Erro ao Gravar. Detalhes: " + err); return; }
+        if (err) { console.log("Erro ao Gravar. Detalhes: " + err); con.destroy(); return; }
         console.log("Localizacao armazenada em Historico");
     });
 
@@ -186,7 +186,7 @@ function salva_mensagem_localizacao(loc_msg, loc_adress, loc_port) {
         "where SN = '" + SN + "'";
 
     con.query(sql, function (err, result) {
-        if (err) { console.log("Erro ao Gravar. Detalhes: " + err); return; }
+        if (err) { console.log("Erro ao Gravar. Detalhes: " + err); con.destroy(); return; }
         console.log("Localizacao Atualizada");
     });
 }
